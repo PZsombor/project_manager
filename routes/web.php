7bf4276c1ba644e9e2194986f419178ff9003947
PZsombor <paulusz.zsombor@gmail.com>
2026-03-14 22:20:40 +0100

    migrations done, controllers renamed, models started

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\RepositoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('repositories', RepositoriesController::class);
+Route::resource('branches', RepositoriesController::class);
+Route::resource('commits', RepositoriesController::class);
+Route::resource('files', RepositoriesController::class);
